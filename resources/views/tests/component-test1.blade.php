f1689363cb35ef1831fa009105c7126f583d682a
Kohei Takahashi <libimff0617@gmail.com>
2022-05-05 13:03:46 +0900

    attribute bag

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,6 @@
     <x-tests.card title="タイトル" content="コンテンツ" :message="$message"/>
 
     <x-tests.card title="タイトルなんです"/>
+
+    <x-tests.card title="CSSを変更したい" class="bg-red-300 shadow-md w-1/4 p-2"/>
 </x-tests.app>
