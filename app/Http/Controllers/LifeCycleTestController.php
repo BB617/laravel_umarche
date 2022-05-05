358e7047067c6d242d4c90fe7c76ecd5cb67168a
Kohei Takahashi <libimff0617@gmail.com>
2022-05-05 17:07:58 +0900

    service provider test

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -6,32 +6,19 @@ use Illuminate\Http\Request;
 
 class LifeCycleTestController extends Controller
 {
+    public function showServiceProviderTest()
+    {
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample, $password, $encrypt->decrypt($password));
+    }
+
     public function showServiceContainerTest()
     {
-        app()->bind('lifeCycleTest', function () {
-            return 'ライフサイクルテスト';
-        });
-
-        $test = app()->make('lifeCycleTest');
-
-        // サービスコンテナなしの場合
-
-        // Messageクラスをインスタンス化
-        $message = new Message();
-        // Messageインスタンスを引数に取り、Sampleクラスをインスタンス化
-        $sample = new Sample($message);
-        // sampleオブジェクトに対して、runメソッドを走らせる。
-        $sample->run();
-
-        // サービスコンテナありの場合
-
-        // app()->bind('呼び出す際の名前', バインドする処理またはオブジェクト)
-        app()->bind('sample', Sample::class);
-        $sample = app()->make('sample');
-        $sample->run();
-
-
-        dd($test, app());
+        dd(app());
     }
 }
 
